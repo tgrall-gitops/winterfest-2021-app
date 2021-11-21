3667ebe689ca95de073224293e8c86992dc429ee
Tugdual Grall <tgrall@github.com>
2021-11-21 15:25:47 +0100

    typo in title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Wintefest 2021' });
+  res.render('index', { title: 'Winterfest 2021' });
 });
 
 module.exports = router;
