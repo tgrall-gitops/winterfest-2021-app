3ad91f72900caa09656dea63eb6e00fa23399f59
Tugdual Grall <tgrall@github.com>
2023-11-03 05:55:05 +0100

    Update index.js (#21)

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Winterfest 2021' });
+  res.render('index', { title: 'Demo November 2023' });
 });
 
 module.exports = router;
